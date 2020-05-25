4b93f4c2cea141799fa0920e1ccc54a0506cf9f8
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-05-25 17:11:09 +0100

    homework

--- models/game.rb
+++ models/game.rb
@@ -5,17 +5,17 @@ class Game
 def self.rock_scissors(hand1, hand2)
 
   if hand1 == "rock" && hand2 == "scissors"
-    return "rock wins"
+    return "Player 1 wins!"
   elsif hand1 == "rock" && hand2 == "paper"
-    return "paper wins"
+    return "Player 2 wins!"
   elsif hand1 == "paper" && hand2 == "rock"
-    return "paper wins"
+    return "Player 1 wins!"
   elsif hand1 == "paper" && hand2 == "scissors"
-    return "scissors win"
+    return "Player 2 wins!"
   elsif hand1 == "scissors" && hand2 == "paper"
-    return "scissors win"
+    return "Player 1 wins!"
   elsif hand1 == "scissors" && hand2 == "rock"
-    return "rock wins"
+    return "Player 2 wins!"
   else "nobody wins, play again"
   end
 end
